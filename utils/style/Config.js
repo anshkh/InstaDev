85d8a89028320a951348b7ee7a0a786021ef4935
vansh khanna <vanshkhanna27@gmail.com>
2021-05-09 22:58:50 +0530

    added camera functionality

---
+++ utils/style/Config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyA-CamuWp0bdP5PZnNW6WAdzgVDjJF3nCg",
+    authDomain: "instademo-bb809.firebaseapp.com",
+    projectId: "instademo-bb809",
+    storageBucket: "instademo-bb809.appspot.com",
+    messagingSenderId: "251989241199",
+    appId: "1:251989241199:web:57b16b1c4bd19243db0c9a",
+    measurementId: "G-SJFH235V0Z"
+  };
